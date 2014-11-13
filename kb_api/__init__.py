176715baa6998ccf49bb2f99c1ea5479a2f84a32
Jonathan Reed <jdreed@mit.edu>
2014-11-13 13:51:04 -0500

    Fix logging in init.py
    Do not call logging.basicConfig in init.py, instead add a NullHandler.
    Do not add a handler on stderr if we can't log to a file

--- kb_api/__init__.py
+++ kb_api/__init__.py
@@ -1,26 +1,23 @@
 import logging
 
-logging.basicConfig()
-
 # Set up logging here
+logger = logging.getLogger('kb_api')
+logger.addHandler(logging.NullHandler())
 
 from .config import APIConfig as config
 
 log_level = config.get('Logging', 'level', 'WARNING')
 log_file = config.get('Logging', 'file', None)
-logsql = False
-try:
-    logsql = str(config.get('Logging', 'log_sql', 'no')).lower() == 'yes'
-except ValueError as e:
-    logging.error("Invalid value for log_sql in config file")
+hdlr = None
 if log_file is not None:
     try:
         hdlr = logging.FileHandler(log_file)
-        hdlr.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(message)s'))
-        logging.getLogger('kb_api').addHandler(hdlr)
-        if logsql:
-            logging.getLogger('sqlalchemy.engine').setLevel(logging.INFO)
-            logging.getLogger('sqlalchemy.engine').addHandler(hdlr)
     except IOError as e:
-        logging.getLogger('kb_api').warning("Warning: Cannot log to file: {0}".format(e))
-    logging.getLogger('kb_api').setLevel(getattr(logging, log_level))
+        logger.error("kb_api: Cannot log to file: {0}".format(e))
+if hdlr is not None:
+    hdlr.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(message)s'))
+    logger.addHandler(hdlr)
+    if config.get('Logging', 'logsql', 'no') == 'yes':
+        logging.getLogger('sqlalchemy.engine').setLevel(logging.INFO)
+        logging.getLogger('sqlalchemy.engine').addHandler(hdlr)
+    logger.setLevel(getattr(logging, log_level))
