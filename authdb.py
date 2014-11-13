35063575a9a22b6e2b5452ae822e984c8cfa53f4
Jonathan Reed <jdreed@mit.edu>
2014-11-13 13:48:50 -0500

    Update authdb.py to use request contexts
    Update authdb.py to use Flask test_request_context() to create the
    tables and access the database.

--- authdb.py
+++ authdb.py
@@ -1,73 +1,91 @@
 #!/usr/bin/python
 
 import logging
+import os
 import sys
 
-logging.basicConfig(level=logging.DEBUG)
-
-from kb_api.config import APIConfig
-from kb_api import auth
 from optparse import OptionParser
 
-def set_callback(option, opt_str, value, parser):
-    optargs = []
-    while len(parser.rargs) > 0 and not parser.rargs[0].startswith('-'):
-        optargs.append(parser.rargs.pop(0))
-    if len(optargs) == 0:
-        parser.error("\n\tAt least one permission must follow '-s space_key'\n"
-                     "\tUse 'NONE' to remove permissions")
-    mode = 0
-    for p in optargs:
+logging.basicConfig(level=logging.DEBUG if os.environ.has_key('KB_API_DEBUG') else logging.ERROR)
+
+from kb_api import admin, auth
+from kb_api.database import db
+
+def ask(question, default=None, answers=None,
+        interrupt=None, want_bool=None):
+    if answers is None:
+        answers = ('y', 'n') 
+    if len(set(answers)) != len(answers):
+        raise ValueError("answers must be unique")
+    if len(answers) < 2:
+        raise ValueError("Must have at 2 answers")
+    if want_bool is None:
+        want_bool = len(answers) == 2
+    elif want_bool and len(answers) != 2:
+        raise ValueError("Can only return boolean with 2 answers")
+    if not all([isinstance(x, basestring) for x in answers]):
+        raise ValueError("Answers must be strings")
+    if default is not None and default not in answers:
+        raise ValueError("default must be in answers")
+    if interrupt is not None and interrupt not in answers:
+        raise ValueError("interrupt must be in answers")
+    prompt = '/'.join(map(lambda x: x.upper() if x == default else x, answers))
+    valid_answers = [x.lower() for x in answers]
+    while True:
         try:
-            mode |= getattr(auth.Permissions, p.upper())
-        except AttributeError:
-            parser.error("Unknown permission '{0}'".format(p))
-    if parser.values.permset is None:
-        parser.values.permset = []
-    parser.values.permset.append((value, mode))
+            ans = raw_input("{0} [{1}] ".format(question, prompt)).strip().lower()
+        except KeyboardInterrupt:
+            if interrupt is not None:
+                ans = interrupt
+            else:
+                raise
+        if len(ans) < 1:
+            if default is None:
+                continue
+            ans = default
+        if ans not in answers:
+            print >>sys.stderr, "Please enter a valid answer: ", answers
+            continue
+        break
+    if want_bool:
+        return answers.index(ans) == 0
+    return ans
 
 parser = OptionParser()
 parser.add_option("-i", dest="init", action="store_true",
                   default=False, help="Initialize database")
-parser.add_option("-a", dest="add", action="store",
-                  help="Add a user and print the key")
-parser.add_option("-k", dest="key", action="store",
-                  help="The user key to set permissions for (with -p)")
-parser.add_option("-p", dest="permset", action="callback", default=None,
-                  callback=set_callback, type="string",
-                  help="Set permissions for SPACE to the positional args")
+parser.add_option("-a", dest="add", action="append",
+                  metavar="user email realname", nargs=3,
+                  help="Add an administrative user")
+parser.add_option("-y", dest="yes", action="store_true",
+                  default=False, help="Do not prompt for confirmation")
+
 (options, args) = parser.parse_args()
 if len(args) > 0:
     parser.error("does not take any positional arguments")
-if options.init:
-    if not all([x is None for x in [options.add,
-                                    options.key,
-                                    options.permset]]):
-        parser.error("-i must be used by itself")
-else:
-    if options.key is not None and options.add is not None:
-        parser.error("-k and -a are mutually exclusive")
-    if options.key is None and options.add is None:
-        parser.error("-a or -k is required")
 
-if options.key is not None and options.permset is None:
-    parser.error("-k requires -p")
+if not options.init and options.add is None:
+    parser.error("At least one option is required.")
 
-
-with auth.AuthenticationContext() as ctx:
+with admin.app.test_request_context():
     if options.init:
-        ctx.create_tables()
-    else:
-        if options.add is not None:
-            user = ctx.add_user(options.add)
-            print "Key for {0} is {1}".format(options.add,
-                                              user.key)
-        else:
-            user = ctx.get_user(options.key)
-        if user is None:
-            print "Error getting user"
-        else:
-            if options.permset is not None:
-                for p in options.permset:
-                    user.set_permission(*p)
-
+        if options.yes or ask("Create the database tables?", default='n'):
+            auth.create_tables()
+            print "Created tables."
+            if not options.add:
+                print >>sys.stderr, "WARNING: No administrators defined.  Please add one."
+    if options.add is not None:
+        for userdata in options.add:
+            uname,email,realname = userdata
+            user = auth.lookup_user(uname)
+            if user is not None:
+                if options.yes or ask("User exists.  Make them admin?", default='y'):
+                    auth.update_db_object(user, ('is_admin',), {'is_admin': True})
+                    print "User updated."
+            elif options.yes or ask("Add {0} ({1}) as administrator?".format(realname, uname), default='y'):
+                auth.add_user(username=uname,
+                              email=email,
+                              real_name=realname,
+                              is_admin=True)
+                print "Added", uname
+sys.exit(0)
