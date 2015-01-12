7615c45a410029856629804d26f7c701a502cbc8
Jonathan Reed <jdreed@mit.edu>
2015-01-12 15:48:38 -0500

    Install package_data in setup.py
    - Add the templates and static directory to package_data so
      they also get installed.

--- setup.py
+++ setup.py
@@ -7,6 +7,10 @@ setup(name='kbapi',
       description='Python modules for the KB API',
       author='Jonathan Reed',
       packages=['kb_api'],
+      package_data={'kb_api': ['templates.admin/*',
+                               'static/images/*',
+                               'static/styles/*',
+                           ]},
       scripts=['authdb.py', 'debug.py'],
       data_files=[('', ['kb_api.wsgi'])]
       )
